f5350fba131ff435b55c294cbcc22a066b5a223a
Mathias Olausson <mathiaso@solidify.se>
2019-04-25 13:04:38 +0200

    improved logging

---
+++ src/WorkItemMigrator/Migration.Common.Log/VersionInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Migration.Common.Log
+{
+    public static class VersionInfo
+    {
+        public static string GetVersionInfo()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetCallingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.ProductVersion;
+            return version;
+        }
+
+        public static string GetCopyrightInfo()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetCallingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return fvi.LegalCopyright;
+        }
+    }
+}
