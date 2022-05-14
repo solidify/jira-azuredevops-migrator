1ffa0f8891e0b9a1a1da7b5717a8c065706618be
Alexander Hjelm <alexander.hjelm@solidify.dev>
2022-05-14 09:26:51 +0200

    Smoke test runs for JiraExport

---
+++ src/WorkItemMigrator/Migration.Tests/SmokeTest.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+
+using JiraExport;
+using WorkItemImport;
+
+using Microsoft.Extensions.CommandLineUtils;
+using static System.Configuration.ConfigurationManager;
+
+namespace Migration.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void JiraExportTest()
+        {
+            string[] argsExport = new string[] {
+                "-u",
+                "alexander.hjelm@solidify.dev",
+                "-p",
+                "XXXXXXXXXXXXXXXXXXXXXXXXX",
+                "--url",
+                "https://XXX.atlassian.net",
+                "--config",
+                "C:\\dev\\jira-azuredevops-migrator\\src\\WorkItemMigrator\\Migration.Tests\\test-config-export.json"
+            };
+            var cmdExport = new JiraCommandLine(argsExport);
+            cmdExport.Run();
+
+            string[] argsImport = new string[] {
+                "-u",
+                "username",
+                "-p",
+                "password",
+                "--url",
+                "https://dev.azure.com/alexanderhjelmsolidify",
+                "--config",
+                "C:\\dev\\jira-azuredevops-migrator\\src\\WorkItemMigrator\\Migration.Tests\\test-config-export.json"
+            };
+            //var cmdImport = new ImportCommandLine(argsImport);
+            //cmdImport.Run();
+        }
+    }
+}
+
+// cd C:\dev\jira-azuredevops-migrator\src\WorkItemMigrator\JiraExport\bin\Debug
+// .\jira-export.exe -u username -p password --url https://dev.azure.com/alexanderhjelmsolidify --config C:\dev\jira-azuredevops-migrator\src\WorkItemMigrator\Migration.Tests\test-config-export.json
