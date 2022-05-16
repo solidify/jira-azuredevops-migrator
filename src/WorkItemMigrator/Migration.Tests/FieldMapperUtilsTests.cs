04ae9bb4c0afa7241a0927b14cf77f17ce5ab94b
Alexander Hjelm <alexander.hjelm@solidify.dev>
2022-05-16 17:33:06 +0200

    Add FieldMapperUtilsTests

---
+++ src/WorkItemMigrator/Migration.Tests/FieldMapperUtilsTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+using JiraExport;
+using WorkItemImport;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture;
+using System;
+using Microsoft.Extensions.CommandLineUtils;
+using Migration.Common.Config;
+using NSubstitute;
+using Common.Config;
+
+namespace Migration.Tests
+{
+    [TestFixture]
+    public class FieldMapperUtilsTests
+    {
+        // use auto fixiture to help mock and instantiate with dummy data with nsubsitute. 
+        private Fixture _fixture;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = new Fixture();
+            _fixture.Customize(new AutoNSubstituteCustomization() { });
+        }
+
+        [Test]
+        public void When_calling_map_remaining_work_with_valid_args_Then_output_is_correct()
+        {
+            object output = FieldMapperUtils.MapRemainingWork("36000");
+            Assert.AreEqual (output, 10);
+        }
+       
+    }
+}
