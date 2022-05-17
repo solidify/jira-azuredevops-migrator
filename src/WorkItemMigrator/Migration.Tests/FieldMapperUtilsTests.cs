7de38b478aee10cb9043f22a8745bb7412355003
Alexander Hjelm <admin@alexander-hjelm.com>
2022-05-17 10:12:28 +0200

    Refactor JiraProvider to interface, MapTitle unit tests (#454)
    * Refactor JiraProvider to interface, MapTitle unit tests
    
    * Use public getters on JiraSettings to expose for testing
    
    * MapTitle tests

--- src/WorkItemMigrator/Migration.Tests/FieldMapperUtilsTests.cs
+++ src/WorkItemMigrator/Migration.Tests/FieldMapperUtilsTests.cs
@@ -1,14 +1,12 @@
 ﻿using NUnit.Framework;
 
 using JiraExport;
-using WorkItemImport;
 using AutoFixture.AutoNSubstitute;
 using AutoFixture;
 using System;
-using Microsoft.Extensions.CommandLineUtils;
-using Migration.Common.Config;
+using Newtonsoft.Json.Linq;
 using NSubstitute;
-using Common.Config;
+using System.Collections.Generic;
 
 namespace Migration.Tests
 {
@@ -17,6 +15,29 @@ namespace Migration.Tests
     {
         // use auto fixiture to help mock and instantiate with dummy data with nsubsitute. 
         private Fixture _fixture;
+
+        private JiraRevision MockRevisionWithParentItem(string issueKey, string revisionSummary)
+        {
+            var provider = _fixture.Freeze<IJiraProvider>();
+
+            JObject remoteIssue = new JObject();
+            remoteIssue.Add("fields", new JObject());
+            remoteIssue.Add("renderedFields", new JObject());
+            remoteIssue.Add("key", issueKey);
+
+            provider.DownloadIssue(default).ReturnsForAnyArgs(remoteIssue);
+            JiraSettings settings = new JiraSettings("userID", "pass", "url", "project");
+            settings.SprintField = "SprintField";
+            provider.GetSettings().ReturnsForAnyArgs(settings);
+
+            JiraItem item = JiraItem.CreateFromRest(issueKey, provider);
+
+            var revision = new JiraRevision(item);
+            revision.Fields = new Dictionary<string, object>();
+            revision.Fields["summary"] = revisionSummary;
+
+            return revision;
+        }
 
         [SetUp]
         public void Setup()
@@ -31,6 +52,56 @@ namespace Migration.Tests
             object output = FieldMapperUtils.MapRemainingWork("36000");
             Assert.AreEqual (output, 10);
         }
-       
+
+        [Test]
+        public void When_calling_map_title_with_empty_args_Then_null_is_returnedt()
+        {
+            var provider = _fixture.Freeze<IJiraProvider>();
+            provider.DownloadIssue(default).Returns(new JObject());
+            var revision = _fixture.Freeze<JiraRevision>();
+
+            (bool, object) output = FieldMapperUtils.MapTitle(revision);
+            (bool, object) expected = (false, null);
+            Assert.AreEqual(output, expected);
+        }
+
+        [Test]
+        public void When_calling_map_title_with_valid_args_Then_output_is_correct()
+        {
+            string issueKey = "issue_key";
+            string summary = "My Summary";
+
+            JiraRevision revision = MockRevisionWithParentItem(issueKey, summary);
+
+            (bool, object) output = FieldMapperUtils.MapTitle(revision);
+            (bool, object) expected = (true, String.Format("[{0}] {1}", issueKey, summary));
+            Assert.AreEqual(output, expected);
+        }
+
+        [Test]
+        public void When_calling_map_title_without_key_with_valid_args_Then_output_is_correct()
+        {
+            string issueKey = "issue_key";
+            string summary = "My Summary";
+
+            JiraRevision revision = MockRevisionWithParentItem(issueKey, summary);
+
+            (bool, object) output = FieldMapperUtils.MapTitleWithoutKey(revision);
+            (bool, object) expected = (true, summary);
+            Assert.AreEqual(output, expected);
+        }
+
+        [Test]
+        public void When_calling_map_title_without_key_with_empty_args_Then_null_is_returnedt()
+        {
+            var provider = _fixture.Freeze<IJiraProvider>();
+            provider.DownloadIssue(default).Returns(new JObject());
+            var revision = _fixture.Freeze<JiraRevision>();
+
+            (bool, object) output = FieldMapperUtils.MapTitleWithoutKey(revision);
+            (bool, object) expected = (false, null);
+            Assert.AreEqual(output, expected);
+        }
+
     }
 }
