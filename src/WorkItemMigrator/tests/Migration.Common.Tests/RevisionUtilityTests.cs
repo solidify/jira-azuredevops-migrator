a1fa24f12820e3e8eeb2a45e09a1f22b6895bd0e
Susanna Persson <zestwerk@gmail.com>
2022-06-15 11:21:54 +0200

    RevisionUtilityTests.cs

--- src/WorkItemMigrator/tests/Migration.Common.Tests/RevisionUtilityTests.cs
+++ src/WorkItemMigrator/tests/Migration.Common.Tests/RevisionUtilityTests.cs
@@ -25,22 +25,34 @@ namespace Migration.Common.Tests
         [Test]
         public void When_calling_nextvaliddeltarev_with_one_param_Then_the_expeced_result_is_returned()
         {
-            DateTime datetime1 = new DateTime();
-            TimeSpan timespan = TimeSpan.FromMilliseconds(50);
+            DateTime datetime = new DateTime();
 
-            Assert.AreEqual(datetime1+timespan, RevisionUtility.NextValidDeltaRev(datetime1));
+            DateTime expected = datetime + TimeSpan.FromMilliseconds(50);
+            DateTime actual = RevisionUtility.NextValidDeltaRev(datetime);
+
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void When_calling_nextvaliddeltarev_with_next_more_than_current_Then_the_expeced_result_is_returned()
         {
             DateTime datetime1 = new DateTime();
-            TimeSpan timespan = TimeSpan.FromMilliseconds(60);
-            DateTime datetime2 = datetime1 + timespan;
+            DateTime datetime2 = datetime1 + TimeSpan.FromMilliseconds(60);
 
-            Assert.AreEqual(datetime1 + TimeSpan.FromMilliseconds(50), RevisionUtility.NextValidDeltaRev(datetime1, datetime2));
+            DateTime expected = datetime1 + TimeSpan.FromMilliseconds(50);
+            DateTime actual = RevisionUtility.NextValidDeltaRev(datetime1, datetime2);
+
+            Assert.AreEqual(expected, actual);
         }
 
+        [Test]
+        public void When_calling_replacehtmlelements_Then_the_expected_result_is_returned()
+        {
+            string expected = "html";
+            string actual = RevisionUtility.ReplaceHtmlElements("html");
+
+            Assert.AreEqual(expected, actual);
+        }
         // ReplaceHtmlElements
         // HasAnyByRefName
     }
