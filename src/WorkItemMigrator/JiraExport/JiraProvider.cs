c0c28991acd1a82ecd9d56558a2bb4bf1bebac1a
Alexander Hjelm <alexander.hjelm@solidify.dev>
2022-08-25 13:16:42 +0200

    Address SonarCloud scan

--- src/WorkItemMigrator/JiraExport/JiraProvider.cs
+++ src/WorkItemMigrator/JiraExport/JiraProvider.cs
@@ -418,13 +418,11 @@ namespace JiraExport
                 if (string.IsNullOrEmpty(customId))
                 {
                     var keyField = (JValue)item.SelectToken("key");
-                    if (keyField != null && keyField.Value != null)
+                    if (keyField != null && keyField.Value != null
+                        && keyField.Value.ToString().ToLower() == propertyName.ToLower())
                     {
-                        if (keyField.Value.ToString().ToLower() == propertyName.ToLower())
-                        {
-                            var idField = (JValue)item.SelectToken("id");
-                            customId = idField.Value.ToString();
-                        }
+                        var idField = (JValue)item.SelectToken("id");
+                        customId = idField.Value.ToString();
                     }
                 }
             }
