92127e60b7113fe6dd6fba489259e6c5564ffe44
Thomas Moerkerken <1309462+moerketh@users.noreply.github.com>
2022-09-14 15:58:20 +0200

    Remove accidentally added using

--- src/WorkItemMigrator/JiraExport/JiraProvider.cs
+++ src/WorkItemMigrator/JiraExport/JiraProvider.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
-using System.Web.Configuration;
 using Atlassian.Jira;
 using Atlassian.Jira.Remote;
 using Migration.Common;
