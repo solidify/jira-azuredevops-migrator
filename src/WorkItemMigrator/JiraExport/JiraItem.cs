a46123347acf26d94a9360b5158272d69da07332
Thomas Moerkerken <1309462+moerketh@users.noreply.github.com>
2022-10-01 17:33:48 +0200

    Remove Epic Link and Parent Link from initial revision

--- src/WorkItemMigrator/JiraExport/JiraItem.cs
+++ src/WorkItemMigrator/JiraExport/JiraItem.cs
@@ -33,13 +33,14 @@ namespace JiraExport
 
         }
 
-        private static List<JiraRevision> BuildRevisions(JiraItem jiraItem, IJiraProvider jiraProvider)
+        public static List<JiraRevision> BuildRevisions(JiraItem jiraItem, IJiraProvider jiraProvider)
         {
             string issueKey = jiraItem.Key;
             var remoteIssue = jiraItem.RemoteIssue;
             Dictionary<string, object> fields = ExtractFields(issueKey, remoteIssue, jiraProvider);
             List<JiraAttachment> attachments = ExtractAttachments(remoteIssue.SelectTokens("$.fields.attachment[*]").Cast<JObject>()) ?? new List<JiraAttachment>();
             List<JiraLink> links = ExtractLinks(issueKey, remoteIssue.SelectTokens("$.fields.issuelinks[*]").Cast<JObject>()) ?? new List<JiraLink>();
+            var epicLinkField = jiraProvider.GetSettings().EpicLinkField;
 
             // save these field since these might be removed in the loop
             string reporter = GetAuthor(fields);
@@ -66,9 +67,28 @@ namespace JiraExport
                 Dictionary<string, object> fieldChanges = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
 
                 var items = change.SelectTokens("$.items[*]")?.Cast<JObject>()?.Select(i => new JiraChangeItem(i));
-
                 foreach (var item in items)
                 {
+                    if (item.Field == "Epic Link" && !string.IsNullOrWhiteSpace(epicLinkField))
+                    {
+                        fieldChanges[epicLinkField] = item.ToString;
+
+                        // undo field change
+                        if (string.IsNullOrWhiteSpace(item.From))
+                            fields.Remove(epicLinkField);
+                        else
+                            fields[epicLinkField] = item.FromString;
+                    }
+                    if (item.Field == "Parent")
+                    {
+                        fieldChanges["parent"] = item.ToString;
+
+                        // undo field change
+                        if (string.IsNullOrWhiteSpace(item.From))
+                            fields.Remove("parent");
+                        else
+                            fields["parent"] = item.FromString;
+                    }
                     if (item.Field == "Link")
                     {
                         var linkChange = TransformLinkChange(item, issueKey, jiraProvider);
@@ -142,7 +162,7 @@ namespace JiraExport
             var author = "NoAuthorDefined";
             if (c.AuthorUser is null)
             {
-                Logger.Log(LogLevel.Warning, $"c.AuthorUser is null in comment revision for jiraItem.Key: '{ jiraItem.Key}'. Using NoAuthorDefined as author. ");
+                Logger.Log(LogLevel.Warning, $"c.AuthorUser is null in comment revision for jiraItem.Key: '{jiraItem.Key}'. Using NoAuthorDefined as author. ");
             }
             else
             {
@@ -258,7 +278,6 @@ namespace JiraExport
             else
                 Logger.Log(LogLevel.Debug, $"No link to undo for '{linkChange.ToString()}'");
         }
-
         private static RevisionAction<JiraLink> TransformLinkChange(JiraChangeItem item, string sourceItemKey, IJiraProvider jira)
         {
             string targetItemKey = string.Empty;
@@ -282,7 +301,6 @@ namespace JiraExport
                 Logger.Log(LogLevel.Error, $"Link change not handled!");
                 return null;
             }
-
             var linkType = jira.GetLinkType(linkTypeString, targetItemKey);
             if (linkType == null)
             {
