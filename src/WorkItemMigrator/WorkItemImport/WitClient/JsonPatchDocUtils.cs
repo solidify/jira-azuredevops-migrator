42deb4f53813149fa8e4083a98c2395ffc23f889
Alexander Hjelm <alexander.hjelm@solidify.dev>
2022-12-09 00:24:22 +0100

    Address code smells

--- src/WorkItemMigrator/WorkItemImport/WitClient/JsonPatchDocUtils.cs
+++ src/WorkItemMigrator/WorkItemImport/WitClient/JsonPatchDocUtils.cs
@@ -4,20 +4,21 @@ using System;
 
 namespace WorkItemImport.WitClient
 {
-    public class JsonPatchDocUtils
+    public static class JsonPatchDocUtils
     {
         public static JsonPatchOperation CreateJsonFieldPatchOp(Operation op, string key, object value)
         {
             if(string.IsNullOrEmpty(key))
             {
-                throw new ArgumentException(key);
-            }
-            if (value == null)
-            {
-                throw new ArgumentException(value.ToString());
+                throw new ArgumentException(nameof(key));
             }
 
-                return new JsonPatchOperation()
+            if (value == null)
+            {
+                throw new ArgumentException(nameof(value));
+            }
+
+            return new JsonPatchOperation()
             {
                 Operation = op,
                 Path = "/fields/" + key,
