e7a7b41a8ccb198ed01ad0c9320843ec0c12f156
Alexander Hjelm <alexander.hjelm@solidify.dev>
2023-10-17 06:27:28 +0200

    Correct image links for Acceptance Criteria

--- src/WorkItemMigrator/WorkItemImport/Agent.cs
+++ src/WorkItemMigrator/WorkItemImport/Agent.cs
@@ -94,6 +94,20 @@ namespace WorkItemImport
                     _witClientUtils.CorrectComment(wi, _context.GetItem(rev.ParentOriginId), rev, _context.Journal.IsAttachmentMigrated);
                 }
 
+                if (wi.Fields.ContainsKey(WiFieldReference.AcceptanceCriteria) && !string.IsNullOrEmpty(wi.Fields[WiFieldReference.AcceptanceCriteria].ToString()))
+                {
+                    Logger.Log(LogLevel.Debug, $"Correcting acceptance criteria on separate revision on '{rev}'.");
+
+                    try
+                    {
+                        _witClientUtils.CorrectAcceptanceCriteria(wi, _context.GetItem(rev.ParentOriginId), rev, _context.Journal.IsAttachmentMigrated);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Log(ex, $"Failed to correct acceptance criteria for '{wi.Id}', rev '{rev}'.");
+                    }
+                }
+
                 _witClientUtils.SaveWorkItemAttachments(rev, wi);
 
                 foreach (string attOriginId in rev.Attachments.Select(wiAtt => wiAtt.AttOriginId))
